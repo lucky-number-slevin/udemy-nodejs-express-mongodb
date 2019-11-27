304d42c06ba131bb7b9831c4b52ca36a5429164b
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-11-27 12:51:03 +0100

    Handle async tour router http request (async function error handling)

--- utils/appError.js
+++ utils/appError.js
@@ -1,12 +1,12 @@
 class AppError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith(4) ? 'fail' : 'error';
-        this.isOperational = true; // indicates that error object is of our custom error class
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith(4) ? 'fail' : 'error';
+    this.isOperational = true; // indicates that error object is of our custom error class
 
-        // Error.captureStackTrace(this, this.constructor);
-    }
+    // Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 module.exports = AppError;
