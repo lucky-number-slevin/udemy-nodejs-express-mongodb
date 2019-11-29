aeaa5e781c024106258673cbe8029e447bb3b144
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-11-29 11:17:14 +0100

    Data modeling

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 // Limit requests from same API
 const limiter = rateLimit({
   // allow 100 request in 1h
-  max: 10,
+  max: 100,
   windowMs: 60 * 64 * 1000,
   message: 'Too many request from this IP. Please try again in an hour!'
 });
@@ -74,7 +74,7 @@ app.all('*', (req, res, next) => {
     `Can't find ${req.originalUrl} on this server`,
     404
   );
-  // NODE: if we pass an arg into next() func, express will automaticlly assume that that arg is an error, and it will
+  // NOTE: if we pass an arg into next() func, express will automaticlly assume that that arg is an error, and it will
   // then skip all other middlewares, and it will pase the error to the middleware function for handling global errors
   next(error);
 });
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -78,7 +79,41 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    // embedded objects (documents)
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      // (2)latitude and (1)longitude
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        // (2)latitude and (1)longitude
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    // child referencing
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -95,6 +130,15 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// embedding user documents on save
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(
+//     async userId => await User.findById(userId)
+//   );
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
