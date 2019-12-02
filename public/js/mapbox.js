c7d57ff4ee73d587735b22a8528a68bc3479bfd1
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-12-02 18:13:37 +0100

    Implement mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,38 @@
+/* esling-disable */
+
+console.log('HELLO FROM THE CLIENT SIDE');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log('LOCATIONS', locations);
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiYmFraXNoYSIsImEiOiJjazNvbzQxb3UwYTVyM2hvNHZvY2k1YW9oIn0.rwR8nAPAoDwXDqGg_eAz8Q';
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/bakisha/ck3oodu4b0emq1cnrm65qmcz0'
+	// center: [19.843957, 45.255297],
+	// zoom: 5.8
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(location => {
+	// create marker
+	const element = document.createElement('div');
+	element.className = 'marker';
+
+	// add marker
+	new mapboxgl.Marker({
+		element,
+		anchor: 'bottom'
+	})
+		.setLngLat(location.coordinates)
+		.addTo(map);
+
+	// extend map bounds to include current location
+	bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+	padding: { top: 200, bottom: 200, left: 100, right: 100 }
+});
