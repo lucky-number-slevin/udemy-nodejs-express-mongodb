8e718bedf0fd4e71f251d71609cf0aec9a2c2eff
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-12-02 15:31:09 +0100

    Implement 'pug' template engine and create first template

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 
+// express supports pug engine (and others), but it needs to be installed
+app.set("view engine", "pug");
+app.set("veiws", path.join(__dirname, "views"));
+
 // 1) GLOBAL MIDDLEWARES
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
+
 // Security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
-  // allow 100 request in 1h
-  max: 100,
-  windowMs: 60 * 64 * 1000,
-  message: 'Too many request from this IP. Please try again in an hour!'
+	// allow 100 request in 1h
+	max: 100,
+	windowMs: 60 * 64 * 1000,
+	message: "Too many request from this IP. Please try again in an hour!"
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -43,42 +52,46 @@ app.use(xss());
 
 // Prevent parameter polution (cleanup query string)
 app.use(
-  hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price'
-    ]
-  })
+	hpp({
+		whitelist: [
+			"duration",
+			"ratingsQuantity",
+			"ratingsAverage",
+			"maxGroupSize",
+			"difficulty",
+			"price"
+		]
+	})
 );
-
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+	req.requestTime = new Date().toISOString();
+	next();
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
+app.get("/", (req, res) => {
+	res.status(200).render("base", {
+		name: "Jon",
+		tour: "Awesome Tour"
+	});
+});
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 // handle all http methods and all unhadled urls
 // this would not be reached if one of the above routes had handled the request
-app.all('*', (req, res, next) => {
-  const error = new AppError(
-    `Can't find ${req.originalUrl} on this server`,
-    404
-  );
-  // NOTE: if we pass an arg into next() func, express will automaticlly assume that that arg is an error, and it will
-  // then skip all other middlewares, and it will pase the error to the middleware function for handling global errors
-  next(error);
+app.all("*", (req, res, next) => {
+	const error = new AppError(
+		`Can't find ${req.originalUrl} on this server`,
+		404
+	);
+	// NOTE: if we pass an arg into next() func, express will automaticlly assume that that arg is an error, and it will
+	// then skip all other middlewares, and it will pase the error to the middleware function for handling global errors
+	next(error);
 });
 
 // CENTRAL MIDDLEWARE FOR HANDLING GLOBAL ERRORS
