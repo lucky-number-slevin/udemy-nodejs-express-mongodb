cf37d1b78a71245c55c089a1d1e1008a308f2280
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-12-02 17:27:04 +0100

    Bunch of template stuff

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
 // express supports pug engine (and others), but it needs to be installed
-app.set("view engine", "pug");
-app.set("veiws", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('veiws', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 
 // Limit requests from same API
@@ -37,12 +38,12 @@ const limiter = rateLimit({
 	// allow 100 request in 1h
 	max: 100,
 	windowMs: 60 * 64 * 1000,
-	message: "Too many request from this IP. Please try again in an hour!"
+	message: 'Too many request from this IP. Please try again in an hour!'
 });
-app.use("/api", limiter);
+app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -54,12 +55,12 @@ app.use(xss());
 app.use(
 	hpp({
 		whitelist: [
-			"duration",
-			"ratingsQuantity",
-			"ratingsAverage",
-			"maxGroupSize",
-			"difficulty",
-			"price"
+			'duration',
+			'ratingsQuantity',
+			'ratingsAverage',
+			'maxGroupSize',
+			'difficulty',
+			'price'
 		]
 	})
 );
@@ -70,21 +71,17 @@ app.use((req, res, next) => {
 	next();
 });
 
-// 3) ROUTES
-app.get("/", (req, res) => {
-	res.status(200).render("base", {
-		name: "Jon",
-		tour: "Awesome Tour"
-	});
-});
+// 3.1) TEMPLATE ROUTES
+app.use('/', viewRouter);
 
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/reviews", reviewRouter);
+// 3.2) API ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // handle all http methods and all unhadled urls
 // this would not be reached if one of the above routes had handled the request
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
 	const error = new AppError(
 		`Can't find ${req.originalUrl} on this server`,
 		404
---
+++ controllers/viewsController.js
@@ -0,0 +1,25 @@
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+	// 1. get tour data from collection
+	const tours = await Tour.find();
+
+	res.status(200).render('overview', {
+		title: 'All Tours',
+		tours
+	});
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+	// 1. get the data for the requested tour (include reviews and quides)
+	const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+		path: 'reviews',
+		fields: 'reivew rating user'
+	});
+	res.status(200).render('tour', {
+		title: tour.name,
+		tour
+	});
+});
