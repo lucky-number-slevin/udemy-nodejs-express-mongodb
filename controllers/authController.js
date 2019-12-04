deb74c12846891a8e868c7fcb941da0f3f0f24d9
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-12-04 11:18:53 +0100

    Implement emails with pug templates

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 const crypto = require('crypto');
 
 const signToken = userId =>
@@ -60,6 +60,9 @@ exports.signup = catchAsync(async (req, res, next) => {
 		password: req.body.password,
 		passwordConfirm: req.body.passwordConfirm
 	});
+
+	const userSettingsUrl = `${req.protocol}://${req.get('host')}/me`;
+	await new Email(newUser, userSettingsUrl).sendWelcome();
 
 	createAndSendResponseWithToken(newUser, 201, res);
 });
@@ -158,15 +161,10 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 	const resetURL = `${req.protocol}://${req.get(
 		'host'
 	)}/api/v1/users/reset-password/${resetToken}`;
-	const message = `Frogot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
-  \nIf you didn't forget your password, please ignore this email!`;
 
 	try {
-		await sendEmail({
-			email: user.email,
-			subject: 'Your password reset token (valid for 10 min)',
-			message
-		});
+		await new Email(user, resetURL).sendPasswordReset();
+
 		res.status(200).json({
 			status: 'success',
 			message: 'Token sent to email!'
--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,63 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1. create transporter
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
+module.exports = class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstName = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `Slevin Kelevra <${process.env.EMAIL_FROM}>`;
+	}
 
-  // 2. define the email options
-  const mailOptions = {
-    from: 'Slevin Kelevra <slevin.kelevra@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    //   html:
-  };
+	createTransport() {
+		if (process.env.NODE_ENV === 'production') {
+			return 1;
+		}
+		console.log(' RETURNING NODEMAILER TRANSPORT');
+		return nodemailer.createTransport({
+			// service: 'Gmail',
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
+	}
 
-  // 3. send the email
-  await transporter.sendMail(mailOptions);
+	async send(template, subject) {
+		// 1. render HTML based on a pug template
+		const html = pug.renderFile(
+			`${__dirname}/../views/emails/${template}.pug`,
+			{
+				firstName: this.firstName,
+				url: this.url,
+				subject
+			}
+		);
+
+		// 2. define email options
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html)
+		};
+
+		// 3. create a transport and send email
+		await this.createTransport().sendMail(mailOptions);
+	}
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to the Natours Family!');
+	}
+
+	async sendPasswordReset() {
+		await this.send(
+			'password-reset',
+			'Your password reset token (valid for 10 minutes)'
+		);
+	}
 };
-
-module.exports = sendEmail;
