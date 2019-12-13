e86f2c5bbf39b3be453ce9a2d9486adacd9a2efa
Milos <roknic.milos.994@gmail.com>
2019-12-13 11:46:05 +0100

    Fix checkout-session url in stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,7 +8,7 @@ const stripe = Stripe('pk_test_YmprdTa57CkXANzE4eUTa8Ml00hvPMwGpC');
 export const bookTour = async tourId => {
   try {
     // 1. get the checkout session from the server (API)
-    const sessionUrl = `https://natours-slevin-kelevra.herokuapp.com/bookings/checkout-session`;
+    const sessionUrl = `https://natours-slevin-kelevra.herokuapp.com/api/v1/bookings/checkout-session`;
     const session = await axios.get(`${sessionUrl}/${tourId}`);
 
     // 2 Create checkout form + charge credit card
